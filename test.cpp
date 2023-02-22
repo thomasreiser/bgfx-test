a4fb82141c85d255ca91bae7b31b779ffb109024
thomasreiser <reiser.thomas@gmail.com>
2023-02-22 10:36:08 +0100

    Add sample code

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <bimg/encode.h>
+
+static inline bx::AllocatorI* getDefaultAllocator() {
+  static bx::DefaultAllocator allocator;
+  return &allocator;
+}
+
+int main(int argc, const char** argv) {
+    bimg::ImageContainer* src = bimg::imageAlloc(getDefaultAllocator(),
+        bimg::TextureFormat::BGRA8, 1, 1, 1, 1, false, false);
+    memcpy(src->m_data, "\xff\x00\x00\xff", 4);
+
+    bimg::ImageContainer* dest = bimg::imageEncode(getDefaultAllocator(),
+        bimg::TextureFormat::BC3, bimg::Quality::Highest, *src);
+
+    bimg::imageFree(src);
+
+    return EXIT_SUCCESS;
+}
